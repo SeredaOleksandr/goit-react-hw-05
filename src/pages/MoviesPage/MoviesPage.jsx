8d90327e113b943df1a449564088fe1fe2143f03
SeredaOleksandr <154976460+SeredaOleksandr@users.noreply.github.com>
2024-08-08 01:45:30 +0300

    correct MoviesPage

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,24 +1,44 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import { searchMovie } from '../../api/movies';
 import s from './MoviesPage.module.css';
 import MovieList from '../../components/MovieList/MovieList';
 import toast, { Toaster } from 'react-hot-toast';
 
 const MoviesPage = () => {
-  const [query, setQuery] = useState('');
+  const [searchParams, setSearchParams] = useSearchParams();
+  const query = searchParams.get('query') || '';
   const [error, setError] = useState(null);
   const [loader, setLoader] = useState(false);
   const [movies, setMovies] = useState([]);
 
-  const handleSubmit = async e => {
+  useEffect(() => {
+    const fetchMovies = async () => {
+      if (!query.trim()) return;
+      setLoader(true);
+      try {
+        const data = await searchMovie(query);
+        setMovies(data.results);
+      } catch (error) {
+        setError(error);
+        toast.error('Error fetching movies.');
+      } finally {
+        setLoader(false);
+      }
+    };
+
+    fetchMovies();
+  }, [query]);
+
+  const handleSubmit = e => {
     e.preventDefault();
 
-    if (!query.trim()) {
+    const searchQuery = e.target.elements.search.value.trim();
+    if (!searchQuery) {
       return toast.error('Please enter a search query.');
     }
 
-    const data = await searchMovie(query);
-    setMovies(data.results);
+    setSearchParams({ query: searchQuery });
   };
 
   return (
@@ -28,14 +48,14 @@ const MoviesPage = () => {
         <input
           className={s.input}
           type="search"
-          value={query}
-          onChange={e => setQuery(e.target.value)}
+          name="search"
+          defaultValue={query}
           placeholder="Search movies"
         />
         <button type="submit">Search</button>
       </form>
       {loader && <p>Loading...</p>}
-      {error && <p className={style.error}>{error}</p>}
+      {error && <p className={s.error}>{error.message}</p>}
 
       <MovieList movies={movies} />
       <Toaster position="top-right" reverseOrder={false} />
